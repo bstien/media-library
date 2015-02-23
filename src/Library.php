b0e33f4f4b1bb0b1f6319407a9223f4d7aafbc11
Bastian Stien <bastian.stien@gmail.com>
2015-02-23 23:06:25 +0100

    Added facade for Library

--- src/Library.php
+++ src/Library.php
@@ -1,6 +1,13 @@
 <?php
 namespace Stien\MediaLibrary;
 
-class Library {
+use Illuminate\Contracts\Config\Repository;
 
+class Library {
+	protected $config;
+
+	function __construct(Repository $config)
+	{
+		$this->config = $config;
+	}
 }
