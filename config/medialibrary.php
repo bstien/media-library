b8d786b7d6f63706224a6ac498be06323e2985f0
Bastian Stien <bastian.stien@gmail.com>
2015-02-23 21:55:40 +0100

    Corrected vendor-name and added blank config-file

---
+++ config/medialibrary.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	/**
+	 * Absolute path to your library.
+	 */
+	'library_path' => '',
+
+	/**
+	 * Your API-key to TheTVDB.com.
+	 * A key can be requested here:
+	 * http://thetvdb.com/?tab=apiregister
+	 */
+	'tvdb_api_key' => '',
+];
