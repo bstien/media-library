bb370da6c7ec614ad6c7091872bc3387f97a64d2
Bastian Stien <bastian.stien@gmail.com>
2015-02-27 14:45:00 +0100

    Removed reference to TvDb. This is now in a separate package

--- src/Content/Episode.php
+++ src/Content/Episode.php
@@ -3,50 +3,12 @@ namespace Stien\MediaLibrary\Content;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Str;
-use Moinax\TvDb\Episode as TvDbEpisode;
 
 class Episode extends Eloquent {
 
 	protected $table = 'medialibrary_episodes';
 	protected $guarded = ['id'];
 	protected $fillable = ['number', 'season', 'serie_id', 'imdb_id', 'name', 'overview', 'first_aired', 'is_downloaded', 'thumbnail'];
-
-	/**
-	 * Create or update an episode in database from TvDbEpisode.
-	 *
-	 * @param TvDbEpisode $e
-	 * @return Episode
-	 */
-	public static function createFromTvDbEpisode(TvDbEpisode $e)
-	{
-		// If season is 0 or less, this is a special episode.
-		// Most likely not aired or special material on DVD.
-		if ( $e->season < 1 ) return;
-
-		// Create or find Episode based on id.
-		$episode = Episode::find($e->id);
-		if ( ! $episode )
-		{
-			$episode = new Episode();
-			$episode->id = $e->id;
-		}
-
-		// Make all public attributes snakecase.
-		// If the key does not exist in $fillable-array,
-		// it will not be related to the object.
-		$information = [];
-		foreach ($e as $k => $v)
-		{
-			$k = Str::snake($k);
-			$information[$k] = $v;
-		}
-
-		$episode->fill($information);
-		$episode->save();
-
-		return $episode;
-	}
 
 
 	/**
--- src/Content/Serie.php
+++ src/Content/Serie.php
@@ -3,8 +3,6 @@ namespace Stien\MediaLibrary\Content;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Str;
-use Moinax\TvDb\Serie as TvDbSerie;
 
 class Serie extends Eloquent {
 
@@ -12,39 +10,6 @@ class Serie extends Eloquent {
 	protected $guarded = ['id'];
 	protected $fillable = ['imdb_id', 'name', 'overview', 'status', 'banner', 'fan_art', 'poster'];
 
-	/**
-	 * Create or update a serie in database from TvDbSerie.
-	 *
-	 * @param TvDbSerie $s
-	 * @return Serie
-	 */
-	public static function createFromTVDbSerie(TvDbSerie $s)
-	{
-		// Create or find Serie based on id.
-		$serie = Serie::find($s->id);
-		if ( ! $serie )
-		{
-			$serie = new Serie();
-			$serie->id = $s->id;
-		}
-
-
-		// Make all public attributes snakecase.
-		// If the key does not exist in $fillable-array,
-		// it will not be related to the object.
-		$information = [];
-		foreach ($s as $k => $v)
-		{
-			$k = Str::snake($k);
-			$information[$k] = $v;
-		}
-
-		// Fill the object, save and return it.
-		$serie->fill($information);
-		$serie->save();
-
-		return $serie;
-	}
 
 	/**
 	 * Define the relationship to Episode
